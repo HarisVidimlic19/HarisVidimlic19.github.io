81557b373629e7be09652e6e596eca951f660b9c
Haris <70179750+HarisVidimlic19@users.noreply.github.com>
2024-02-11 20:13:50 -0500

    Switched to optimized website for Git Pages

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+// Import CSS
+import '../css/icomoon.css'
+import '../css/flaticon.css'
+import '../css/glider.min.css'
+import '../css/style.css'
+
+// Import Images
+// import '../images/about.webp'
+// import '../images/bg_1.webp'
+// import '../images/bg_3.webp'
+// import '../images/project-1.webp'
+// import '../images/project-2.webp'
+// import '../images/project-3.webp'
+// import '../images/project-4.webp'
+// import '../images/project-5.webp'
+// import '../images/project-6.webp'
+
+// Import JS
+import './glider.min.js'
+import './main.js'
