b26716cab93f71e3a3db6c5f8e4a70608058da86
Haris <70179750+HarisVidimlic19@users.noreply.github.com>
2024-09-19 21:32:05 -0400

    Update scrape.js

--- node/scrape.js
+++ node/scrape.js
@@ -2,24 +2,22 @@ import { launch } from 'puppeteer';
 import { writeFileSync } from 'fs';
 
 (async () => {
-    // Launch a headless browser instance
+    // Launch a headless browser instance and open new page
     const browser = await launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
         headless: "true",
         timeout: 0
     });
-
-    // Open a new page
     const page = await browser.newPage();
 
-    // Navigate to the URL you want to scrape
+    // Navigate to the URL to scrape
     await page.goto('https://phys.org/physics-news/sort/popular/1w/');
 
-    // Wait for any necessary page content to load (you may need to adjust the selector)
-    await page.waitForSelector('.sorted-news-list');
+    // Wait for any necessary page content to load
+    await page.waitForSelector('.sorted-news-list.px-3');
 
     // Extract the content of the div container
-    const content = await page.$eval('.sorted-news-list', (div) => div.innerHTML);
+    const content = await page.$eval('.sorted-news-list.px-3', (div) => div.innerHTML);
 
     // Extract the content of the div container
     writeFileSync('data/output.html', content);
